f13e83d34b1a5b99b00de5e7d5228617afd57f77
tellEmBringMeMyMoney <crogenesto@yandex.ru>
2026-05-03 10:12:22 +0300

    tryna beat them checks

--- src/main/ru/practicum/moviehub/http/MoviesServer.java
+++ src/main/ru/practicum/moviehub/http/MoviesServer.java
@@ -2,7 +2,6 @@ package ru.practicum.moviehub.http;
 
 import com.sun.net.httpserver.HttpServer;
 import ru.practicum.moviehub.store.MoviesStore;
-import ru.practicum.moviehub.http.MoviesHandler;
 
 import java.io.IOException;
 import java.net.InetSocketAddress;
